e85fba98c6049e56acf1c5e9866a921d8ce095f8
Alan Yeh <blowfihsyeh@gmail.com>
2013-03-07 22:26:21 -0500

    Add scrip to copy vim related files to /home/user/.vim

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ function cp_to_home {
     local file_name=${src_file##*/} 
     local dest_file=$HOME/.${file_name}
     if [ -f ${dest_file} ]; then 
-        echo "File ${dest_file} already exist, backup old file."
+        #echo "File ${dest_file} already exist, backup old file."
         cp ${dest_file} ${dest_file}.old 
     fi
 
@@ -14,9 +14,22 @@ function cp_to_home {
 
 
 #
-# Copy vimrc
+# Copy doc files to home directory.
 #
+echo -ne "Copy dot files..."
 cp_to_home vim/vimrc
 cp_to_home bash/bashrc
 cp_to_home git/gitconfig
+echo -ne "\t\t[OK]\n"
+
+
+#
+# copy vim plugins and doc
+#
+echo -ne "Copy vim related fils..."
+mkdir -p $HOME/.vim
+cp -af vim/doc $HOME/.vim
+cp -af vim/plugin $HOME/.vim
+cp -af vim/colors $HOME/.vim
+echo -ne "\t[OK]\n"
 
